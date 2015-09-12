47a96085e59e1d94034a5829f97ae70d663d0483
Stuart Sandine <stuajc@gmail.com>
2015-09-12 13:15:28 -0500

    Make indentation two spaces

--- test/index.js
+++ test/index.js
@@ -2,16 +2,16 @@ var assert = require('assert');
 var add = require('../src/index.js').add;
 
 describe('add()', function() {
-	it('should be rly good at math', function() {
-		assert.equal(add(1, 1), 2);
-	});
+  it('should be rly good at math', function() {
+    assert.equal(add(1, 1), 2);
+  });
 });
 
 describe('this starter proj', function() {
-	it('should work ok with es6 because babel', function() {
-		assert.doesNotThrow(function() {
-			var x = () => 5;
-			var y = `oh hai`;
-		});
-	});
+  it('should work ok with es6 because babel', function() {
+    assert.doesNotThrow(function() {
+      var x = () => 5;
+      var y = `oh hai`;
+    });
+  });
 });
